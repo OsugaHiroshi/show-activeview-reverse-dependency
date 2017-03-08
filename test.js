208e5e19b9cbfc6f287cbf9bf24c865b85fbbde7
osuga.hiroshi <osuga.hiroshi@moneyforward.co.jp>
2017-03-08 16:51:30 +0900

    renameしてテスト書く

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 const test = require('ava');
 const {
-  convertToActiveViewPartialSpecifiedString
+  convertToActiveViewPartialSpecifiedString,
+  findFilesByGitGrep
 } = require('./lib');
 
 test('convertToActiveViewPartialSpecifiedString', t => {
   const result = convertToActiveViewPartialSpecifiedString('A/B/C/_D.ext.ext', 'A/B');
   t.is(result, 'C/D');
+});
+
+test('findFilesByGitGrep', async t => {
+  const files = await findFilesByGitGrep('A/a', './fixture');
+  t.is(files.length, 1);
+  t.is(files[0], 'fixture/B/_b.txt');
 });
